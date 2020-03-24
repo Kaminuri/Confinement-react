9c08f1fe55ff7e09e4e8c5b43a8b581f3af5a019
dleveneur <davyleveneur@gmail.com>
2020-03-24 15:11:33 +0400

    material design

---
+++ src/component/Layout/Layout.js
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  Route,
+  Switch,
+  Redirect,
+  withRouter,
+} from "react-router-dom";
+import classnames from "classnames";
+
+// styles
+import useStyles from "./styles";
+
+// components
+import Header from "../Header";
+import Sidebar from "../Sidebar";
+
+// pages
+import Tables from "../../pages/tables";
+import Icons from "../../pages/icons";
+
+// context
+import { useLayoutState } from "../../context/LayoutContext";
+
+function Layout(props) {
+  var classes = useStyles();
+
+  // global
+  var layoutState = useLayoutState();
+
+  return (
+    <div className={classes.root}>
+        <>
+          <Header history={props.history} />
+          <Sidebar />
+          <div
+            className={classnames(classes.content, {
+              [classes.contentShift]: layoutState.isSidebarOpened,
+            })}
+          >
+            <div className={classes.fakeToolbar} />
+            <Switch>
+              <Route path="/app/tables" component={Tables} />
+              <Route
+                exact
+                path="/app/ui"
+                render={() => <Redirect to="/app/ui/icons" />}
+              />
+              <Route path="/app/ui/icons" component={Icons} />
+            </Switch>
+          </div>
+        </>
+    </div>
+  );
+}
+
+export default withRouter(Layout);
