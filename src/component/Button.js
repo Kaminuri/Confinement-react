8d1d737f8b8b75da4066d09b3312748b339114d9
Akey <renaldbooz@live.fr>
2020-03-24 09:08:19 +0100

    Component Test

---
+++ src/component/Button.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+class Button extends Component {
+  render() {
+    import Button from 'component/Button';
+  }
+}
+export default Button; // Don’t forget to use export default!
---
+++ src/component/header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Header extends React.Component {
+    render() {
+       return (
+          <div>
+             <h1>Header</h1>
+          </div>
+       );
+    }
+  }
+  export default Header;
