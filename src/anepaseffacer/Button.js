6a1bd2d5c783b9c0e0b4b3ba4515026a94d08f0b
Akey <renaldbooz@live.fr>
2020-03-26 10:49:55 +0100

    delete useless file

--- src/anepaseffacer/Button.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-class Button extends Component {
-  render() {
-    import Button from 'component/Button';
-  }
-}
-export default Button; // Don’t forget to use export default!
