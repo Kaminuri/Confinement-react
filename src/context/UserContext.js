e601e99e70b235be4a2e21ba17e04b9ba0b702d5
Your Name <matt-picard@hotmail.fr>
2020-03-30 11:39:56 +0200

    dd

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,5 +1,5 @@
 import React from "react";
-//import axios from 'axios';
+import axios from 'axios';
 
 var UserStateContext = React.createContext();
 var UserDispatchContext = React.createContext();
